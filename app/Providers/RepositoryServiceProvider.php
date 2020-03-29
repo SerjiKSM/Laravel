238f6e7e705bc9003dcc016e9fe5a1b21e01b30b
SerjiK <serjikcygnati@gmail.com>
2020-03-29 13:46:45 +0300

    8-Attributes Management part 2

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -5,8 +5,8 @@ namespace App\Providers;
 use App\Contracts\CategoryContract;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\CategoryRepository;
-//use App\Contracts\AttributeContract;
-//use App\Repositories\AttributeRepository;
+use App\Contracts\AttributeContract;
+use App\Repositories\AttributeRepository;
 use App\Contracts\BrandContract;
 use App\Repositories\BrandRepository;
 //use App\Contracts\ProductContract;
@@ -18,7 +18,7 @@ class RepositoryServiceProvider extends ServiceProvider
 {
     protected $repositories = [
         CategoryContract::class         =>          CategoryRepository::class,
-//        AttributeContract::class        =>          AttributeRepository::class,
+        AttributeContract::class        =>          AttributeRepository::class,
         BrandContract::class            =>          BrandRepository::class,
 //        ProductContract::class          =>          ProductRepository::class,
 //        OrderContract::class            =>          OrderRepository::class,
