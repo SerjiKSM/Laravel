199ab8afac7fe3805caef93ae7271de916ca6619
SerjiK <serjikcygnati@gmail.com>
2020-03-31 00:32:02 +0300

    8-Attributes Management part 3

--- database/migrations/2020_03_26_220133_create_attribute_values_table.php
+++ database/migrations/2020_03_26_220133_create_attribute_values_table.php
@@ -18,7 +18,8 @@ class CreateAttributeValuesTable extends Migration
             $table->unsignedBigInteger('attribute_id')->unsigned();
             $table->foreign('attribute_id')->references('id')->on('attributes');
             $table->text('value');
-            $table->decimal('price', 2)->nullable();
+//            $table->decimal('price', 2)->nullable();
+            $table->double('price', 2)->nullable();
             $table->timestamps();
         });
     }
